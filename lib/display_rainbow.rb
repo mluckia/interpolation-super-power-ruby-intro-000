db748459e6e0a69ac67a4ba301c090f8d2841f21
Michelle Hauser <michellehauser5@gmail.com>
2020-02-20 01:28:11 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,4 @@ def display_rainbow(colors)
   puts "R: #{colors[0]}, Y: #{colors[1]}, G: #{colors[2]}, B: #{colors[3]}, I: #{colors[4]}, V: #{colors[5]}"
 end
 
-display_rainbow
+display_rainbow(colors)
